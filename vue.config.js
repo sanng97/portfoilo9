ba451b0e0ffeed49d9a196c066a68a071ac26db6
sanng97 <sanng97@naver.com>
2022-10-19 22:02:38 +0900

    change

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath:"/portfolio9",
+  publicPath:"/portfoilo9",
   transpileDependencies: [
     'vuetify'
   ]
